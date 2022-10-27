a441b95d6a9a9eab3206a41cfb64544e95c66ce8
Quan Pham <doraemonfanclub@gmail.com>
2022-10-28 00:08:18 +0700

    Prevent multiple instance of the app and keep the PC awake

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
+#include <Windows.h>
 #include <QApplication>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <QMessageBox>
+#include <QtCore/QCommandLineParser>
+#include <QtCore/QCommandLineOption>
+#include <QtCore/QDir>
+#include <QSharedMemory>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +15,16 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QApplication app(argc, argv);
+    QSharedMemory sharedMemory;
+
+    // https://stackoverflow.com/questions/34445164/qt5-preventing-another-instance-of-the-application-doesnt-work-anymore
+    sharedMemory.setKey("fyi-memory-key");
+    if (sharedMemory.create(1) == false)
+    {
+        QMessageBox::warning(NULL, "Info", "Another instance of the app is already running.");
+        app.exit();
+        return 0;
+    }
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
@@ -19,5 +35,12 @@ int main(int argc, char *argv[])
     }, Qt::QueuedConnection);
     engine.load(url);
 
-    return app.exec();
+    // https://learn.microsoft.com/vi-vn/windows/win32/api/winbase/nf-winbase-setthreadexecutionstate?redirectedfrom=MSDN
+    SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
+    int resp = app.exec();
+
+    // Reset the state and allow the system to sleep normally:
+    SetThreadExecutionState(ES_CONTINUOUS);
+
+    return resp;
 }
